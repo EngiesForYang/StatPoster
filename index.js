6b0377192651bde3d84bd9740961883844ebbd06
Aditya Varma Alluri <aalluri7@gmail.com>
2019-11-10 21:50:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,95 @@
+let props = {};
+
+
+
+function renderStats(fullData) {
+  if (fullData) {
+    const {
+      stats,
+      geo,
+      geoType
+    } = fullData;
+    return (
+      `
+        <h2>${geo}</h3>
+        <div>Area type: ${geoType}</div>
+        ${Object.keys(stats).map(lable=>`<div>${lable}: ${stats[lable]}</div>`).join('')}
+       `
+    );
+  } else {
+    return '';
+  }
+
+}
+
+function renderHtml(newProps) {
+
+  const {
+    fullData
+  } = newProps;
+  props = newProps;
+
+  document.getElementById("app").innerHTML = `
+<h1>Hello Yang!</h1>
+<div>
+  <form onsubmit='return getStats(event);'>
+      <input name='location' id='inputlocation' type='text'/>
+      <input type='submit' />
+  </form>
+  ${renderStats(fullData)}
+</div>
+`;
+
+}
+
+async function getincomefromzip(zip) {
+  const data = await fetch(`https://factfinder.census.gov/rest/communityFactsNav/nav?N=0&_t=${Date.now()}&log=t&spotlightId=ALL&searchTerm=${zip}`);
+  const json = await data.json()
+  //console.log(json);
+  return json;
+};
+
+async function loadData(term) {
+  const data = await getincomefromzip(term);
+  console.log(data)
+  const fullData = data.CFMetaData;
+  const stats = fullData.measuresAndLinks.allMeasures.reduce((o, c) => {
+    const getValue = (list) => {
+      if (list && list.length) {
+        const val = list.reduce((o, c) => {
+          if (c.value && o.length < 1) {
+            o = c.value;
+          }
+          return o;
+        }, '')
+        return val;
+      }
+    }
+    o[c.label] = c.value ? c.value : getValue(c.list)
+    return o;
+  }, {})
+  fullData.stats = stats;
+  console.log({
+    stats
+  })
+  renderHtml({
+    ...props,
+    fullData
+  })
+
+}
+
+
 function getStats(e) {
   e.preventDefault();
   try {
-    console.log({ e });
+    const location = document.querySelector('#inputlocation')
+    const searchTerm = location.value;
+    loadData(searchTerm)
   } catch (e) {
     console.error(e);
   }
   return false;
 }
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Yang!</h1>
-<div>
-  <form onsubmit='return getStats(event);'>
-      <input type='text'/>
-      <input type='submit' />
-  </form>
-</div>
-`;
+renderHtml(props);
